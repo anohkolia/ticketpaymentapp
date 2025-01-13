e875001fa228a20d00eeda548e6d8bf4e2655543
anohkolia <colectionmac.com>
2025-01-13 23:57:47 +0100

    implémentation de router index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import EventPage from '../pages/EventPage.vue';
+import CheckoutPage from '../pages/CheckoutPage.vue';
+import OrderConfirmation from '../pages/OrderConfirmation.vue';
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+export const router = createRouter({
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      component: EventPage
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/checkout',
+      component: CheckoutPage
     },
-  ],
-})
-
-export default router
+    {
+      path: '/confirmation',
+      component: OrderConfirmation
+    }
+  ]
+});
